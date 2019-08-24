e0f603fed410e1035e62ab650411eb3d08cbc863
Shubham Subedi <intabcsc@gmail.com>
2019-08-24 23:27:23 +0545

    edit and delete

--- django-project/taskmate/todolist_app/views.py
+++ django-project/taskmate/todolist_app/views.py
@@ -35,3 +35,32 @@ def about(request):
     }
     #return HttpResponse("Welcome to todolist")
     return render(request, 'about.html', context)
+
+def delete_task(request, task_id):
+    task = TaskList.objects.get(pk=task_id)
+    task.delete()
+    return redirect('todolist')
+
+def edit_task(request, task_id):
+    if request.method == "POST":
+        task = TaskList.objects.get(pk=task_id)
+        form = TaskForm(request.POST or None, instance = task)
+        if form.is_valid():
+            form.save()
+        messages.success(request, ("Task Edited!"))
+        return redirect('todolist')
+    else:
+        task_object = TaskList.objects.get(pk=task_id)
+        return render(request, 'edit.html', {'task_object':task_object})
+
+def complete_task(request, task_id):
+    task = TaskList.objects.get(pk=task_id)
+    task.done = True
+    task.save()
+    return redirect('todolist')
+
+def pending_task(request, task_id):
+    task = TaskList.objects.get(pk=task_id)
+    task.done = False
+    task.save()
+    return redirect('todolist')
